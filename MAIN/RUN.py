510681cf3a02764fc0fa2b28e0cf1ae94e64b7bc
M2theJJ <64149913+M2theJJ@users.noreply.github.com>
2021-01-17 20:26:46 +0100

    Update RUN.py

--- MAIN/RUN.py
+++ MAIN/RUN.py
@@ -9,7 +9,7 @@ from MAIN import Settings, Conversions, QUANTIZER, W_A
 
 
 'b_s = 32, epochs = 200, data_augemtation = true, num_classes = 10 (CIFAR10), substract_pixel_mean = true'
-settings = Settings.training_parameters(32, 1, True, 10, True, None, None) #returns bs, ep, da, nc, spm, qwp, qap
+settings = Settings.training_parameters(32, 200, True, 10, True, None, None) #returns bs, ep, da, nc, spm, qwp, qap
 data = Settings.data(10, settings)  #returns x_train, y_train, x_test, y_test, x_train_mean, input_shape, dataset
 #resnet1 = RESNET.ResNet(1, 3, data, settings, q=False)
 #W_A.activations_weights(resnet2, data, settings, "Resnetv1")
